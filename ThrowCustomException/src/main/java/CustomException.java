d3198cc8ba394c70e818c2733fa9507ffb0fc1de
sporty786 <michelle.e.tracy@gmail.com>
2023-01-03 04:55:53 +0000

    Saved progress on ThrowCustomException 2023-01-03 04:55:53 +0000

--- ThrowCustomException/src/main/java/CustomException.java
+++ ThrowCustomException/src/main/java/CustomException.java
@@ -5,4 +5,11 @@
  * Potentially, this class could also override some the behavior of Exception.
  */
 public class CustomException extends Exception{
+    public CustomException(){
+        super();
+    }
+
+    public CustomException(String message){
+        super(message);
+    }
 }

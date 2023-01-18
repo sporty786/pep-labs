b0989f0056d1f5434404c9e160f090b608b6e765
sporty786 <michelle.e.tracy@gmail.com>
2023-01-18 02:43:17 +0000

    Saved progress on SelectWhereAndOr 2023-01-18 02:43:17 +0000

--- SelectWhereAndOr/problem1.sql
+++ SelectWhereAndOr/problem1.sql
@@ -1,1 +1,1 @@
-SELECT * FROM employee WHERE firstName = 'Steve' AND salary > 75000;
+SELECT * FROM employee WHERE first_name = 'Steve' AND salary > 75000;
--- SelectWhereAndOr/problem3.sql
+++ SelectWhereAndOr/problem3.sql
@@ -1,1 +1,1 @@
-SELECT * FROM employee WHERE salary > 50000 AND firstName <> 'Steve';
+SELECT * FROM employee WHERE salary > 50000 AND first_name <> 'Steve';

8b039d76c2a61bb23235e3d263abbac361709a9f
sporty786 <michelle.e.tracy@gmail.com>
2023-02-01 22:13:13 +0000

    Saved progress on Pyramid 2023-02-01 22:13:13 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,12 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String pyramid = "";
+        String star = "*";
+        for (int i = 0; i < n; i++){
+            for (int l = 0; l < i; l++){pyramid += star;}
+            pyramid += "\n";
+        }
+        return pyramid;
     }
 }

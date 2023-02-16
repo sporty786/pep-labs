b17c36a6a4c78649f0142127f2071e4570a7d5be
sporty786 <michelle.e.tracy@gmail.com>
2023-02-16 02:50:13 +0000

    Saved progress on WordCount 2023-02-16 02:50:13 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,13 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int numSpaces = 0;
+        for (int i = 0; i < in.length(); i++){
+            if (in.charAt(i) == ' '){
+                numSpaces++;
+                if (i == in.length()-1){i--;}
+            }
+        }
+        return numSpaces + 1;
     }
 }

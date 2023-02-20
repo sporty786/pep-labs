6116ba33e877b48bc37a0d5814058d1dbf5fb72d
sporty786 <michelle.e.tracy@gmail.com>
2023-02-20 04:45:24 +0000

    Saved progress on Isogram 2023-02-20 04:45:24 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,28 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Map<Character, Integer> charFreq = new HashMap<>();
+        String chars = "";
+        for (int i = 0; i < str.length(); i++){
+            char letter = str.charAt(i);
+            if (charFreq.get(letter) != null){charFreq.put(letter, charFreq.get(letter));}
+            else {
+                charFreq.put(letter, 1);
+                chars += letter;
+            }
+        }
+        int biggest;
+        char recur;
+        if (str.length() <= 0){return ' ';}
+        recur = chars.charAt(0);
+        biggest = charFreq.get(recur);
+        for (int i = 1; i < chars.length(); i++){
+            int freq = charFreq.get(chars.charAt(i));
+            if (freq > biggest){
+                biggest = freq;
+                recur = chars.charAt(i);
+            }
+        }
+        return recur;
     }
 }

64abb180e8aed1ce6cfbc829c97ba0f4f51459eb
sporty786 <michelle.e.tracy@gmail.com>
2023-02-24 23:29:34 +0000

    Saved progress on StringIsPalindrome 2023-02-24 23:29:34 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,10 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        int length = str.length();
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) != str.charAt(length - 1 - i)){return false;}
+        }
+        return true;
     }
 }
